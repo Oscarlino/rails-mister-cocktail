50b54b71559db08aeae512df6a9bebe370c6d8c9
thibault de combarieu <tdecomba@gmail.com>
2016-11-17 20:01:07 +0100

    first draft

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+
+
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+url = "https://www.atelier-cocktail.com/recettes-cocktails-top25/"
+html_file = open(url)
+doc = Nokogiri::HTML(html_file)
+
+
+
+
+doc.search("#cocktails-listing li").each do |cocktail|
+  image_url = cocktail.search("img")[0].attr("src")
+  name = cocktail.search(".top25")[0].text
+  if cocktail.search("p")[0]
+    description = cocktail.search("p")[0].text.strip
+  else
+    description = "no desc"
+  end
+  Cocktail.create!(name: name, description: description, image_url: image_url)
+end
+
+ingredients = %w(lemon ice mint leaves redbull jagermeister sugar tonic gin rhum)
+ingredients.each { |ingredient| Ingredient.create(name: ingredient) }
+
+
